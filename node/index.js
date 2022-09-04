6f82b29c9ae19eb9e36328732d887ef874ce9f18
greysoh <greysoh@pm.me>
2022-09-04 19:28:23 -0400

    feature: Ports code to index.js.

--- node/index.js
+++ node/index.js
@@ -1,18 +1,19 @@
-import os from "https://deno.land/x/dos@v0.11.0/mod.ts";
+const os = require("os");
+const fs = require("fs");
 
 function stringParser(str) {
   return str.split(str[0])[1];
 }
 
-export function compile(path, disableWelcome) {
-  const delimiter = os.platform == "windows" ? "\\" : "/";
+function compile(path, disableWelcome) {
+  const delimiter = os.platform() == "win32" ? "\\" : "/";
 
   const rootPathArr = path.split(delimiter);
   rootPathArr.pop();
 
   const rootPath = rootPathArr.join(delimiter);
 
-  const file = Deno.readTextFileSync(path);
+  const file = fs.readFileSync(path, "utf-8");
 
   const fileSplit = file.split("\n");
   let isRegularRequireEnabled = false;
@@ -42,4 +43,51 @@ export function compile(path, disableWelcome) {
   }
 
   return newLines.join("\n");
+}
+
+module.exports = compile;
+
+if (require.main == module) {
+  const path = process.argv[2];
+
+  if (!path) {
+    console.error("error: File not specified.")
+    process.exit(1);
+  }
+  
+  if (!fs.existsSync(path)) {
+    console.error("error: File does not exist!");
+  }
+  
+  if (path.endsWith(".js")) {
+    const data = compile(path);
+  
+    fs.writeFileSync("a.out.js", data)
+  } else if (path.endsWith(".json")) {
+    const file = JSON.parse((path));
+  
+    for (const i of file.projects) {
+      console.log("Compiling '%s' -> '%s'", i.path, i.out);
+  
+      const data = typeof i.type == "string" && i.type == "static" ? fs.readFileSync(i.path) : compile(i.path);
+      
+      const dirPath = i.out.split("/");
+      dirPath.pop();
+  
+      try {
+        fs.mkdirSync(dirPath.join("/"), { recursive: true });
+      } catch (e) {
+        console.warn("WARN <Step::Mkdir>:", e);
+      }
+  
+      try {
+        fs.writeFileSync(i.out, data);
+      } catch (e) {
+        console.error("ERR <Step::Write>:", e);
+      }
+      
+    }
+  } else {
+    console.error("error: Invalid file type!");
+  }
 }
